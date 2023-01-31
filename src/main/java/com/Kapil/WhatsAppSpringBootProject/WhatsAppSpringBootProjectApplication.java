22641c0e13ab81c5dae3a2670b93cfc4c5201502
KAPIL  BENIWAL <kapilbeniwal2813@gmail.com>
2023-01-31 14:57:53 +0530

    WhatsApp Project Complete

---
+++ src/main/java/com/Kapil/WhatsAppSpringBootProject/WhatsAppSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Kapil.WhatsAppSpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppSpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatsAppSpringBootProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/Kapil/WhatsAppSpringBootProject/WhatsAppSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kapil.WhatsAppSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
